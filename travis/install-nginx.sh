1200d77247f56c3f7e31d313258352c716248196
Sebastian Grodzicki <sebastian@shwrm.com>
2016-06-25 11:29:22 +0200

    Nginx

--- travis/install-nginx.sh
+++ travis/install-nginx.sh
@@ -39,8 +39,7 @@ tpl "$DIR/php-fpm.conf" "$PHP_FPM_CONF"
 # Build the default nginx config files.
 tpl "$DIR/nginx.conf" "$DIR/nginx/nginx.conf"
 tpl "$DIR/fastcgi_params" "$DIR/nginx/fastcgi_params"
-rm $DIR/nginx/sites-enabled/*
-tpl "$DIR/001-api.shwrm.net" "$DIR/nginx/sites-enabled/001-api.shwrm.net"
+tpl "$DIR/001-api.shwrm.net" "$DIR/nginx/sites-enabled/default-site.conf"
 
 # Start nginx.
 nginx -c "$DIR/nginx/nginx.conf"
